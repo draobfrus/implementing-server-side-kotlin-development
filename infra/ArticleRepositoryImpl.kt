f87fa45fec10d38e2e656480ea7f18732101a82d
draobfrus <aelfci5am@gmail.com>
2025-12-27 22:05:58 +0900

    feat: 作成済み記事リポジトリの具象クラス

--- infra/ArticleRepositoryImpl.kt
+++ infra/ArticleRepositoryImpl.kt
@@ -1,6 +1,38 @@
 /**
  * 作成済記事リポジトリの具象クラス
  *
+ * @property namedParameterJdbcTemplate
  */
 @Repository
-class ArticleRepositoryImpl : ArticleRepository
+class ArticleRepositoryImpl(val namedParameterJdbcTemplate: NamedParameterJdbcTemplate) : ArticleRepository {
+    override fun findBySlug(slug: Slug): Either<ArticleRepository.FindBySlugError, CreatedArticle> {
+        val sql = """
+            SELECT
+                articles.slug
+                , articles.title
+                , articles.body
+                , articles.description
+            FROM
+                articles
+            WHERE
+                slug = :slug
+        """.trimIndent()
+        val articleMapList = namedParameterJdbcTemplate.queryForList(sql, MapSqlParameterSource().addValue("slug", slug.value))
+
+        /**
+         * DB から作成済記事が見つからなかった場合、早期 return
+         */
+        if (articleMapList.isEmpty()) {
+            return ArticleRepository.FindBySlugError.NotFound(slug = slug).left()
+        }
+
+        val articleMap = articleMapList.first()
+
+        return CreatedArticle.newWithoutValidation(
+            slug = Slug.newWithoutValidation(articleMap["slug"].toString()),
+            title = Title.newWithoutValidation(articleMap["title"].toString()),
+            description = Description.newWithoutValidation(articleMap["description"].toString()),
+            body = Body.newWithoutValidation(articleMap["body"].toString()),
+        ).right()
+    }
+}
